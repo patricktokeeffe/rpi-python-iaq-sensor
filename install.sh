974da559b49f4fc156a97cf50e4328dc2f589c87
Patrick O'Keeffe <pokeeffe@wsu.edu>
2016-08-13 16:36:38 -0800

    Rename pressure logging service after device model

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 
 # pressure-logger
 echo "Installing BMP logging service executable..."
-cp scripts/pressure-logger.py /usr/sbin/pressure-logger
-chmod +x /usr/sbin/pressure-logger
+cp scripts/bmp180-logger.py /usr/sbin/bmp180-logger
+chmod +x /usr/sbin/bmp180-logger
 mkdir -p /etc/wsn
-cp etc/wsn/pressure-logger.conf /etc/wsn/
+cp etc/wsn/bmp180-logger.conf /etc/wsn/
 
 echo "Registering BMP180 logging service..."
-cp etc/systemd/system/pressure-logger.service /etc/systemd/system/
+cp etc/systemd/system/bmp180-logger.service /etc/systemd/system/
 
 echo "Enabling BMP180 logging service start at boot..."
-systemctl enable pressure-logger.service
+systemctl enable bmp180-logger.service
 
 echo "Starting BMP logging service..."
-systemctl restart pressure-logger.service
+systemctl restart bmp180-logger.service
 
